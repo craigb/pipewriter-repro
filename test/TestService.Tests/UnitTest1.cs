5ad6f67053febf454116965afda10f4febef51e3
Craig Boucher <Craig.Boucher@microsoft.com>
2024-11-27 12:05:16 -0600

    Add unit test

--- test/TestService.Tests/UnitTest1.cs
+++ test/TestService.Tests/UnitTest1.cs
@@ -1,10 +1,26 @@
 namespace TestService.Tests;
 
+using Microsoft.AspNetCore.Mvc.Testing;
+
 [TestClass]
-public class UnitTest1
+public class UnitTest1 
 {
-    [TestMethod]
-    public void TestMethod1()
+    private WebApplicationFactory<Program> _testHost;
+
+    [TestInitialize]
+    public void TestInitialize()
     {
+        // Create a new WebApplicationFactory with the Program class
+        _testHost = new WebApplicationFactory<Program>().WithWebHostBuilder(builder =>
+        {
+        });
+    }
+
+    [TestMethod]
+    public async Task TestMethod1()
+    {
+        var httpClient = _testHost.CreateDefaultClient();
+        using var response = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, "/weatherforecast"));
+        Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
     }
 }
